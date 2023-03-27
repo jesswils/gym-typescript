6ea91f54ea35e15e2cc4f1e7c3ee3c6fd8240b78
Jess <jessica.wilson@jisc.ac.uk>
2023-03-27 15:11:08 +0100

    amend navbar css

--- src/components/navbar/Link.tsx
+++ src/components/navbar/Link.tsx
@@ -12,7 +12,9 @@ const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
 
   return (
     <AnchorLink
-      className={`${selectedPage === lowerCasePage ? 'grey-green' : ''}"
+      className={`${selectedPage === lowerCasePage ? 'grey-green' : ''}
+      transition duration-500 hover:text-blue-green text-xl text-neutral-50
+      font-medium tracking-wider
     `}
       href={`#${lowerCasePage}`}
       onClick={() => setSelectedPage(lowerCasePage)}
